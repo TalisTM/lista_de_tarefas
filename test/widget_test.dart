86c12dcb566b839dd18347b2962bb173dcde6d7c
TalisTM <talismarchioro@gmail.com>
2020-10-27 21:45:05 -0300

    comiit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,9 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:lista_de_tarefas/main.dart';
-
+//teste
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
